a6517f85a3cb83f2b11046614db0df7f028a8eac
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2023-01-19 16:14:30 +0100

    Added tabs in the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,39 +2,55 @@ import React from 'react';
 import { Navbar } from '../Navbar';
 import './Header.scss';
 import './Title-bar.scss';
-
 export const Header = () => (
   <div className="header">
     <div className="header__top">
       <Navbar />
     </div>
 
-    <div className="header__bottom title-bar">
-      <div className="title-bar__top">
-        <h1 className="title">
-          Småstadens kommun
-        </h1>
+    <div className="header__mid title-bar">
+      <h1 className="title">
+        Småstadens kommun
+      </h1>
 
-        <form className="find-person">
-          <div className="field">
-            <label className="label" htmlFor="person-search" />
+      <form className="find-person">
+        <div className="field">
+          <label className="label" htmlFor="person-search" />
 
-            <div className="control">
-              <input
-                type="text"
-                id="person-search"
-                placeholder="Sök"
-                className="input"
-                name="query"
-              />
-            </div>
+          <div className="control">
+            <input
+              type="text"
+              id="person-search"
+              placeholder="Sök"
+              className="input is-dander"
+              name="query"
+            />
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
+    </div>
 
-      <div className="title-bar__bottom">
+    <div className="header__bottom header-tabs">
+      <a
+        className="header-tab"
+        href="#"
+      >
+        Organisationshierarki
+      </a>
 
-      </div>
+      <a
+        className="header-tab"
+        href="#"
+      >
+        Schemainställningar
+      </a>
+
+      <a
+        className="header-tab"
+        href="#"
+      >
+        Kontoinställningar
+      </a>
     </div>
   </div>
 );

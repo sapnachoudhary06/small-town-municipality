8d72c3e6f4d33bcc64792afd09f347d06996599a
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2023-01-19 16:52:45 +0100

    added background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { KommunPage } from "./components/KommunPage";
+import './App.scss';
 
 export const App = () => {
   return (
     <>
       <Header />
-      <KommunPage />
+
+      <div className="app">
+        <KommunPage />
+      </div>
     </>
   );
 };
